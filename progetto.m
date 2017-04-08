05ff73c13956d7f8b6f5b83792a2d11195e8fb48
Ferruvich <ferruvich@gmail.com>
2017-04-08 16:43:39 +0200

    Fixed problems with dark images

--- progetto.m
+++ progetto.m
@@ -1,7 +1,7 @@
-%function progetto(input_image)
+function progetto(image_file)
 	close all;
 	% Mostriamo l'immagine
-    input_image = imread('../UBIRIS_800_600/Sessao_2/201/Img_201_2_5.jpg');
+    input_image = imread(image_file);
 	figure, imshow(input_image);
 
 	%Inizio preprocessing
@@ -9,39 +9,23 @@
 	% Controllando le immagini, abbiamo notato che la parte rossa
 	% si presta meglio alla segmentazione (rimane più nitida)
 	im_red = input_image(:,:,1);
-    %figure, imshow(im_red);
 
 	% Applichiamo un filtro gaussiano
 	gaussian_filter = fspecial('gaussian', [800 600], 5);
 	double_im = double(im_red);
 	im_filter = imfilter(double_im, gaussian_filter);
-    %%figure, imshow(im_filter);
 
     %Edge detection
     %Usiamo un canny edge detector
     edges = edge(im_filter, 'canny', 0.1,5);
-    %figure, imshow(edges);
        
     % Usiamo imfindcircles per trovare oggetti circolari
     % all''interno dell''immagine
     
-    % Pupilla
-    [centers, radii, metric] = imfindcircles(edges, [35, 60], 'Sensitivity', .99);
-    dim = size(centers);
-    if dim(1) > 1
-        centers = centers(1,:);
-        radii = radii(1);
-    end
-    %%[accum, centers, radii] = CircularHough_Grd(edges, [40 70]);
-    viscircles(centers, radii, 'EdgeColor', 'r');
-    %%figure, imshow(accum);
-    %DrawCircle(accum)
-    
     % Iride, fatto in modo diverso perchè imfindcircles non funziona
     % altrimenti
     im2 = rgb2gray(input_image);
-    im3 = im2bw(im2, 0.55);
-    %%figure, imshow(im3);
+    im3 = im2bw(histeq(im2), 0.55);
     [centersI, radiI] = imfindcircles(im3, [160, 220], 'Sensitivity', .99, 'ObjectPolarity', 'dark');
     dim = size(centersI);
     if dim(1) > 1
@@ -50,8 +34,23 @@
     end
     viscircles(centersI, radiI, 'EdgeColor', 'b');
     
+    % Pupilla
+    [centers, radii] = imfindcircles(edges, [35, 60], 'Sensitivity', .95);
+    dim = size(centers);
+    if dim(1) > 1
+        for i = 1:dim(1)
+            diffY = (centers(i,1)-centersI(1))^2;
+            diffX = (centers(i,2)-centersI(2))^2;
+            if (diffX < 1000) && (diffY < 1000)
+                centers = centers(i,:);
+                radii = radii(i);
+                break;
+            end
+        end
+    end
+    viscircles(centers, radii, 'EdgeColor', 'r');
 
-    % Post processing - blank eye version
+    % Post processing - versione bianca
     imageSize = size(input_image);
     coordinatesIris = [centersI(2), centersI(1), radiI];
     coordinatesPupil = [centers(2), centers(1), radii];
@@ -65,7 +64,7 @@
     finalCropped = croppedImageIris-croppedImagePupil;
     figure, imshow(finalCropped);
     
-    %Post processing - Full eye version
+    %Post processing - crop della zona dell'iride nell'immagine originale
     croppedImage = uint8(zeros(size(input_image)));
     finalCropped = finalCropped./255;
     croppedImage(:,:,1) = input_image(:,:,1).*finalCropped;
